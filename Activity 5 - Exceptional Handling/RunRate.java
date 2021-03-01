733a68518ef92eff1e6fdcbbea3f0d9438933798
Ajeet mishra <78849047+ajeetmishra-ltts@users.noreply.github.com>
2021-03-01 09:06:18 +0530

    Update RunRate.java

--- Activity 5 - Exceptional Handling/RunRate.java
+++ Activity 5 - Exceptional Handling/RunRate.java
@@ -1,31 +1,25 @@
 import java.io.*;
-import java.util.*;
-
 public class RunRate {
-    Scanner scan = new Scanner(System.in);
-    int runs, balls;
-    float runRate;
+	
+	public static void main(String args[])
+	{
+		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+		int total_runs,total_overs;
+		
+		try
+		{
+			System.out.println("Enter total runs: ");
+			total_runs = Integer.parseInt(reader.readLine());
+			System.out.println("Enter total Overs faced: ");
+			total_overs = Integer.parseInt(reader.readLine());
+			float result = total_runs/total_overs;
+			System.out.println("The Current Run Rate is: "+result);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
 
-    public void input() {
-        try {
-            System.out.println("Enter the total runs scored: ");
-            runs = scan.nextInt();
-            System.out.println("Enter the total overs faced ");
-            balls = scan.nextInt();
-        } catch (NumberFormatException e) {
-            System.out.println("Error Code: " + e);
-            System.exit(0);
-        }
-    }
-
-    public void compute() {
-        runRate = runs / balls;
-        System.out.println("Current Run Rate : " + runRate);
-    }
-
-    public static void main(String[] args) {
-        RunRate obj = new RunRate();
-        obj.input();
-        obj.compute();
-    }
 }

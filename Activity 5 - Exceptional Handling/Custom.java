4cd69d2eed82e8f872a1b5e62aacbcf2b703360d
Ajeet mishra <78849047+ajeetmishra-ltts@users.noreply.github.com>
2021-03-01 09:08:23 +0530

    Add files via upload

---
+++ Activity 5 - Exceptional Handling/Custom.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Custom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter Player Name: ");
+		try
+		{
+			String name = reader.readLine();
+			System.out.println("Enter Player Age: ");
+			int age = Integer.parseInt(reader.readLine());
+			if(age<18)
+			{
+				throw new ArithmeticException("InvalidAgeRange");
+			}
+			else
+			{
+				System.out.println("Player Name: "+name);
+				System.out.println("Player Age: "+age);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+
+}

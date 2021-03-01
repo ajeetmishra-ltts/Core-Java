afb7cc761f9f4b8be3d6b4fa272b296af9d7ec05
Ajeet mishra-Ltts <ajeetkumar.mishra@ltts.com>
2021-03-01 16:36:20 +0530

    Core Java

---
+++ Activity 6 - Inner Class/StaticInnerClass.java
@@ -0,0 +1,16 @@
+class MotherBoard {
+static class USB{
+       int usb2 = 2;
+       int usb3 = 1;
+       int getTotalPorts(){
+           return usb2 + usb3;
+       }
+   }
+
+}
+public class StaticInnerClass {
+   public static void main(String[] args) {
+       MotherBoard.USB usb = new MotherBoard.USB();
+       System.out.println("Total Ports = " + usb.getTotalPorts());
+   }
+}
